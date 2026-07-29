99c0dff479fbe80dc7235e7146c1d0f0c2eae3e6
Hamzah <hamzah.sama@gmail.com>
2026-07-29 18:18:50 +0700

    refaracor #2

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,8 +1,8 @@
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 
-import sessions from "./routes/sessions";
-import chat from "./routes/chat";
+import sessions from "./routes/sessions.js";
+import chat from "./routes/chat/index.js";
 
 const app = new Hono();
 
